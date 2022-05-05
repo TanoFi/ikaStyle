0465c7dda5c5097c8e106fd9cba0f004f8a0b990
TanoshiSegawa <tsubotsubo877@gmail.com>
2022-05-05 20:48:08 +0900

    Add : カスタムビューGearPowerReceptorImageViewとカスタム属性receptorKindを追加

---
+++ app/src/main/java/com/example/ikastyle/UI/GearPowerReceptorImageView.java
@@ -0,0 +1,51 @@
+package com.example.ikastyle.UI;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.DragEvent;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.ikastyle.Common.Const.GearPowerResourceId;
+import com.example.ikastyle.R;
+
+public class GearPowerReceptorImageView extends AppCompatImageView implements View.OnDragListener {
+    private int gearPowerKind;
+    //サブギアなら0, アタマのメインギア1, フクのメインギア2, クツのメインギア3が入る
+    private int receptorKind;
+
+    public GearPowerReceptorImageView(Context context, AttributeSet attrs){
+        super(context, attrs);
+
+        // カスタム属性receptorKindの値を取得しフィールド変数gearPowerKindに入れる
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.GearPowerReceptorImageView, 0, 0);
+        receptorKind = typedArray.getInt(R.styleable.GearPowerReceptorImageView_receptorKind, receptorKind);
+
+        // onDragリスナーをセット
+        this.setOnDragListener(this::onDrag);
+    }
+
+    public void setGearPowerKind(int gearPowerKind){
+        this.gearPowerKind = gearPowerKind;
+
+        // セットされたGearPowerKindの画像に差し替え
+        this.setImageResource(GearPowerResourceId.gearPowerResourceIdMap.get(gearPowerKind));
+    }
+
+    @Override
+    public boolean onDrag(View view, DragEvent dragEvent){
+        if(dragEvent.getAction() == DragEvent.ACTION_DROP){
+            GearPowerImageView dragView = (GearPowerImageView) dragEvent.getLocalState();
+
+            int gearPowerKind = dragView.getGearPowerKind();
+            // 一部のギアパワーは特定のギアのメインギアにしか付けられないためこのようなif条件を入れている(e.g.ラストスパートはアタマのメインギアにしか付けられない)
+            if(gearPowerKind / 100 == 0 || gearPowerKind / 100 == receptorKind){
+                // フィールド変数gearPowerKindをセットしながら、セットしたKindのギア画像に差し替え
+                ((GearPowerReceptorImageView)view).setGearPowerKind(gearPowerKind);
+            }
+        }
+        return true;
+    }
+}
