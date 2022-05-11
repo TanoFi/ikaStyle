40206172c0ff4670cbb27fff288009378183cbca
TanoFi <104102967+TanoFi@users.noreply.github.com>
2022-05-11 23:48:00 +0900

    fix : ギアのマスターテーブル修正

--- app/src/main/java/com/example/ikastyle/Entity/HeadGear.java
+++ app/src/main/java/com/example/ikastyle/Entity/HeadGear.java
@@ -1,18 +1,20 @@
 package com.example.ikastyle.Entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-
-import io.reactivex.annotations.NonNull;
 
 @Entity(tableName = "MAST_HEAD_GEAR", primaryKeys = {"id", "language_code"})
 public class HeadGear {
     @ColumnInfo(name = "id")
+    @NonNull
     public int id;
 
     @ColumnInfo(name = "language_code")
+    @NonNull
     public int languageCode;
 
     @ColumnInfo(name = "name")
+    @NonNull
     public String name;
 }
--- app/src/main/java/com/example/ikastyle/Entity/ShoesGear.java
+++ app/src/main/java/com/example/ikastyle/Entity/ShoesGear.java
@@ -1,16 +1,20 @@
 package com.example.ikastyle.Entity;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 
 @Entity(tableName = "MAST_SHOES_GEAR", primaryKeys = {"id", "language_code"})
 public class ShoesGear {
     @ColumnInfo(name = "id")
+    @NonNull
     public int id;
 
     @ColumnInfo(name = "language_code")
+    @NonNull
     public int languageCode;
 
     @ColumnInfo(name = "name")
+    @NonNull
     public String name;
 }
