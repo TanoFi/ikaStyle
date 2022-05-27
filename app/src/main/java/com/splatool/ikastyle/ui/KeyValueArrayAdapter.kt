634c15de25e98de4723d97ce4167303613073488
TanoFi <104102967+TanoFi@users.noreply.github.com>
2022-05-28 07:21:40 +0900

    clean : KeyValueArrayAdapterのDataBinding対応

--- app/src/main/java/com/splatool/ikastyle/ui/KeyValueArrayAdapter.kt
+++ app/src/main/java/com/splatool/ikastyle/ui/KeyValueArrayAdapter.kt
@@ -1,5 +1,6 @@
 package com.splatool.ikastyle.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -7,56 +8,53 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.splatool.ikastyle.R
 import com.splatool.ikastyle.common.Util
 import com.splatool.ikastyle.common.const.NumberPlace
+import com.splatool.ikastyle.databinding.SpinnerListDropdownItemBinding
+import com.splatool.ikastyle.databinding.SpinnerListItemBinding
 
 class KeyValueArrayAdapter(
     context: Context,
     layoutResourceId: Int,
-    keyValues: ArrayList<Pair<Int, String>>
+    private val keyValues: ArrayList<Pair<Int, String>>
 ) : ArrayAdapter<Pair<Int, String>>(context, layoutResourceId, keyValues) {
     private val inflater: LayoutInflater = LayoutInflater.from(context)
 
-    private class ViewHolder(view : View) {
-        val weaponImageView: ImageView = view.findViewById(R.id.imageView_weapon)
-        val weaponNameView: TextView = view.findViewById(R.id.textView_weaponName)
-    }
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding= DataBindingUtil.inflate(inflater, R.layout.spinner_list_item, parent, false) as SpinnerListItemBinding
+
         // ブキIdとブキ名を取得
         val absoluteWeaponId: Int = getItem(position)!!.first
         val weaponName = getItem(position)!!.second
 
-        // spinner_list_item.xmlのレイアウトをインスタンス化する
-        val view = inflater.inflate(R.layout.spinner_list_item, null)
-        val holder = ViewHolder(view)
         if (absoluteWeaponId % NumberPlace.MAIN_PLACE == 0) { // メイン単位の項目が選択されている場合
             // 表示する画像がないのでimageViewを消す
-            holder.weaponImageView.visibility = View.GONE
+            binding.imageViewWeapon.visibility = View.GONE
         } else { // カスタマイズ単位の項目が選択されている場合
             // imageViewに当該のブキ画像セット
-            holder.weaponImageView.setImageResource(Util.getWeaponResourceId(absoluteWeaponId))
+            binding.imageViewWeapon.setImageResource(Util.getWeaponResourceId(absoluteWeaponId))
         }
         // textViewにブキ名セット
-        holder.weaponNameView.text = weaponName
-        return view
+        binding.textViewWeaponName.text = weaponName
+        return binding.root
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val binding = DataBindingUtil.inflate(inflater, R.layout.spinner_list_dropdown_item, parent, false) as SpinnerListDropdownItemBinding
+
         val absoluteWeaponId: Int = getItem(position)!!.first
         val weaponName = getItem(position)!!.second
 
         // spinner_list_dropdown_item.xmlのレイアウトをインスタンス化する
-        val view = inflater.inflate(R.layout.spinner_list_dropdown_item, null)
-        val holder = ViewHolder(view)
         if (absoluteWeaponId % NumberPlace.MAIN_PLACE == 0) {
-            holder.weaponImageView.visibility = View.GONE
+            binding.imageViewWeapon.visibility = View.GONE
         } else {
-            holder.weaponImageView.setImageResource(Util.getWeaponResourceId(absoluteWeaponId))
+            binding.imageViewWeapon.setImageResource(Util.getWeaponResourceId(absoluteWeaponId))
         }
-        holder.weaponNameView.text = weaponName
-        return view
+        binding.textViewWeaponName.text = weaponName
+        return binding.root
     }
 
     fun resetKeyValues(keyValues: ArrayList<Pair<Int, String>>){
